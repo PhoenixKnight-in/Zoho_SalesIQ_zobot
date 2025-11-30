967ad507c50a115948f51f26259ee0ec88873b56
PhoenixKnight-in <phoenixknight18012007@gmail.com>
2025-11-30 17:27:37 +0530

    Changes in package

--- frontend/my-react-app/src/ZohoSalesIQ.jsx
+++ frontend/my-react-app/src/ZohoSalesIQ.jsx
@@ -5,18 +5,19 @@ const ZohoSalesIQ = () => {
     window.$zoho = window.$zoho || {};
     window.$zoho.salesiq = window.$zoho.salesiq || { ready: function () {} };
 
+    // prevent adding script twice
     if (document.getElementById("zsiqscript")) return;
 
-    const s = document.createElement("script");
-    s.id = "zsiqscript";
-    s.defer = true;
-    s.src =
+    const script = document.createElement("script");
+    script.id = "zsiqscript";
+    script.defer = true;
+    script.src =
       "https://salesiq.zohopublic.com/widget?wc=siq4a76cf1202d4615d40ef407e062d686c00293ada420e9026730e70ada3f1a14b";
 
-    document.body.appendChild(s);
+    document.body.appendChild(script);
   }, []);
 
-  return null;
+  return null; // component renders nothing
 };
 
 export default ZohoSalesIQ;
